46fb3c263a2b3f57a8019aa0f5aa147441b333e5
Alexrjj <alexsoares2517@gmail.com>
2020-09-22 18:30:38 -0300

    Cria form salvar evento

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Evento = require('./events/Event');
 
 // Database Connection
 const connection = require('./database/connection');
+const { default: slugify } = require('slugify');
 connection.authenticate().then(() => {
   console.log('DB Ok');
 }).catch((msgErro) => {
@@ -21,22 +22,47 @@ app.use(bodyParser.json());
 // Arquivos estáticos
 app.use(express.static('public'));
 
-// Rota padrão
+
+// --- Rotas ---
+// Padrão
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Rota criar evento
+// Criar evento
 app.get('/criar', (req, res) => {
   res.render('criaEvento');
 });
 
-// Rota listar eventos
+// Listar eventos
 app.get('/evento', (req, res) => {
   res.render('evento');
 });
 
+// Salvar evento
+app.post('/salvar', (req, res) => {
+  let name = req.body.name;
+  let local = req.body.local;
+  let date = req.body.date;
+  let time = req.body.time;
+
+  if (name != undefined) {
+    Evento.create({
+      name: name,
+      local: local,
+      date: date,
+      time: time,
+      slug: slugify(name, { lower: true }),
+    }).then(() => {
+      res.redirect('/')
+    })
+  }else {
+    alert('Verifique os campos!');
+    res.redirect('/criar');
+  }
+});
+
 // Servidor Express
 app.listen(3000, () => {
-  console.log(`Server started on port ${3000}`);
+  console.log(`Server started on port 3000`);
 });
